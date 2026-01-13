90c272b2594868f148b3f9579e0811410601fb20
Thibaut Chantrel <thibaut.chantrel0102@gmail.com>
2026-01-13 19:14:12 +0100

    [Feat] - Ajout d'un marker custom

--- src/components/GpxViewer.tsx
+++ src/components/GpxViewer.tsx
@@ -1,18 +1,67 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-gpx'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Tooltip } from 'react-leaflet'
+import { renderToString } from 'react-dom/server'
+import { PawPrint } from 'lucide-react'
 
-/**
- * Fix icônes Leaflet (problème classique avec Webpack / Next.js)
- */
+export type GpxPoint = {
+  id?: string | number
+  lat: number
+  lng: number
+  label: string
+  color?: string
+}
+
+const PawMarker = ({ point }: { point: GpxPoint }) => {
+  const icon = useMemo(() => {
+    const color = point.color || '#F5EEE0'
+
+    const iconHtml = renderToString(
+      <div className="relative flex items-center justify-center">
+        <div
+          className="w-8 h-8 rounded-full flex items-center justify-center shadow-lg text-white transform hover:scale-110 transition-transform"
+          style={{ backgroundColor: color }}
+        >
+          <PawPrint size={20} fill="currentColor" fillOpacity={0.2} />
+        </div>
+        {/* Pointe */}
+        <div
+          className="absolute -bottom-1 w-3 h-3 rotate-45"
+          style={{ backgroundColor: color }}
+        />
+      </div>
+    )
+
+    return L.divIcon({
+      html: iconHtml,
+      className: 'bg-transparent',
+      iconSize: [40, 40],
+      iconAnchor: [20, 44],
+      popupAnchor: [0, -44],
+    })
+  }, [point.color])
+
+  return (
+    <Marker position={[point.lat, point.lng]} icon={icon}>
+      <Tooltip
+        direction="top"
+        offset={[0, -45]}
+        opacity={1}
+        className="text-sm"
+      >
+        {point.label}
+      </Tooltip>
+    </Marker>
+  )
+}
+
 function fixLeafletIcons() {
   //eslint-disable-next-line @typescript-eslint/no-explicit-any
   delete (L.Icon.Default.prototype as any)._getIconUrl
-
   L.Icon.Default.mergeOptions({
     iconRetinaUrl:
       'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
@@ -23,12 +72,8 @@ function fixLeafletIcons() {
   })
 }
 
-/**
- * Couche GPX
- */
 function GpxLayer({ url }: { url: string }) {
   const map = useMap()
-
   useEffect(() => {
     if (!url) return
     //eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -36,70 +81,67 @@ function GpxLayer({ url }: { url: string }) {
       async: true,
       marker_options: {
         startIconUrl:
-          'https://raw.githubusercontent.com/mpetazzoni/leaflet-gpx/master/pin-icon-start.png',
+          'https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/pin-icon-start.png',
         endIconUrl:
-          'https://raw.githubusercontent.com/mpetazzoni/leaflet-gpx/master/pin-icon-end.png',
+          'https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/pin-icon-end.png',
         shadowUrl:
-          'https://raw.githubusercontent.com/mpetazzoni/leaflet-gpx/master/pin-shadow.png',
+          'https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/pin-shadow.png',
       },
       polyline_options: {
-        color: '#2563eb',
-        opacity: 0.85,
-        weight: 4,
+        color: '#355F4A',
+        opacity: 0.8,
+        weight: 5,
         lineCap: 'round',
       },
     })
-
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
     gpxLayer
-      //eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .on('loaded', (e: any) => {
-        map.fitBounds(e.target.getBounds())
-      })
-      //eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .on('error', (e: any) => {
-        console.error('Erreur chargement GPX:', e)
-      })
+      .on('loaded', (e: any) => map.fitBounds(e.target.getBounds()))
       .addTo(map)
-
-    // 🔥 nettoyage obligatoire
     return () => {
       map.removeLayer(gpxLayer)
     }
   }, [url, map])
-
   return null
 }
 
 interface GpxViewerProps {
   fileId?: string
   customUrl?: string
+  points?: GpxPoint[]
 }
 
-/**
- * Viewer GPX
- */
-export default function GpxViewer({ fileId, customUrl }: GpxViewerProps) {
+export default function GpxViewer({
+  fileId,
+  customUrl,
+  points = [],
+}: GpxViewerProps) {
   useEffect(() => {
     fixLeafletIcons()
   }, [])
 
   if (!fileId && !customUrl) return null
-
   const gpxUrl = customUrl ?? `/api/files/${fileId}`
 
   return (
-    <div className="w-full h-[400px] rounded-lg overflow-hidden border shadow-sm relative">
+    <div className="w-full h-full min-h-100 rounded-lg overflow-hidden border shadow-sm relative z-0">
       <MapContainer
-        center={[46.603354, 1.888334]} // centre France
+        center={[46.603354, 1.888334]}
         zoom={5}
         scrollWheelZoom={false}
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution="&copy; OpenStreetMap"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
         <GpxLayer url={gpxUrl} />
+
+        {/* BOUCLE SUR LA LISTE DES POINTS */}
+        {points.map((point, index) => (
+          <PawMarker key={point.id || index} point={point} />
+        ))}
       </MapContainer>
     </div>
   )
