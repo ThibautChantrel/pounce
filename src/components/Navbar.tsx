8492f83818ea51bc5d53ad68d95e538ca9682895
Thibaut Chantrel <thibaut.chantrel0102@gmail.com>
2026-02-12 21:16:37 +0100

    [Feat] - Connexion cachée des utilisateurs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import { Settings } from 'lucide-react'
 import { Link } from '@/navigation'
 import AuthModal from './authModal'
 
-export default async function Navbar() {
+export default async function Navbar({ showConnexionStatus = false }) {
   const t = await getTranslations('Navbar')
   const session = await auth()
 
@@ -20,28 +20,29 @@ export default async function Navbar() {
         <PawPrint className="w-8 h-8" />
         <span className="text-xl font-bold tracking-tight">{t('brand')}</span>
       </Link>
-
-      <div>
-        {session ? (
-          <div className="flex items-center gap-4">
-            <span className="text-sm font-medium">
-              {session.user?.name || session.user?.email}
-            </span>
-            {session.user.role === 'ADMIN' && (
-              <Link href="/admin">
-                <Button variant="ghost">
-                  <Settings className="w-8 h-8" />
-                </Button>
-              </Link>
-            )}
-            <LogoutButton />
-          </div>
-        ) : (
-          <AuthModal
-            trigger={<Button variant="primary">{t('authButton')}</Button>}
-          />
-        )}
-      </div>
+      {showConnexionStatus && (
+        <div>
+          {session ? (
+            <div className="flex items-center gap-4">
+              <span className="text-sm font-medium">
+                {session.user?.name || session.user?.email}
+              </span>
+              {session.user.role === 'ADMIN' && (
+                <Link href="/admin">
+                  <Button variant="ghost">
+                    <Settings className="w-8 h-8" />
+                  </Button>
+                </Link>
+              )}
+              <LogoutButton />
+            </div>
+          ) : (
+            <AuthModal
+              trigger={<Button variant="primary">{t('authButton')}</Button>}
+            />
+          )}
+        </div>
+      )}
     </nav>
   )
 }
